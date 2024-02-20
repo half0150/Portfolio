5d0ee399fa56e1ead1bb4acb0961ecae127da3be
Halfdan Karlsen Monie <half0150@edu.sde.dk>
2024-02-20 22:16:44 +0100

    ThemeSwitcher is still not working

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Provider } from './providers'
+import Providers from "./provider";
+import ThemeSwitcher from "./ThemeSwitcher";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+        <ThemeSwitcher />
+        {children}</Providers></body>
     </html>
   );
 }
