4cbea516ee7b420a012214e45cf49faba8dae85b
Halfdan Karlsen Monie <half0150@edu.sde.dk>
2024-02-29 08:44:02 +0100

    Fixed spelling errors on resume patge

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -6,14 +6,14 @@ export default function index() {
           <div className="text-slate-300">
             <h3 className="text-3xl font-semibold mb-6 text-teal-600">Resume</h3>
             <p className="mb-4">Mit navn er Halfdan Karlsen Monie, og jeg er 18 år gammel. Jeg bor i Odense sammen med mine forældre og søskende.</p>
-            <p className="mb-4">Jeg er i gang med min uddnalese på Syddansk Erhvervsskole, hvor jeg studerer til datatekniker med specialisering i programmering.</p>
+            <p className="mb-4">Jeg er i gang med min uddannelse på Syddansk Erhvervsskole, hvor jeg studerer til datatekniker med specialisering i programmering.</p>
             <p className="mb-4">I min fritid nyder jeg at tilbringe tid sammen med mine venner og have det sjovt med dem. Jeg bruger også gerne min fritid på at programmere.</p>
             <p className="mb-4">Jeg søger aktivt en læreplads, da jeg ønsker at udvikle mine færdigheder som datatekniker. Jeg er i øjeblikket i skolepraktik, men jeg foretrækker at få erfaring direkte fra en virksomhed.</p>
             <p className="mb-4">Jeg afsluttede mit grundforløb den 27. juni 2023.</p>
             <p className="mb-4">Mit mål er at arbejde i en virksomhed, hvor jeg kan udvikle mine evner som datatekniker og få indblik i, hvordan det er at arbejde i en IT-virksomhed.</p>
             <img src="../../cv_billede.jpg" alt="Halfdan Monie" className="w-80 h-80 rounded-full mb-6 ml-auto" /> {}
             <p className="mb-4">Jeg arbejder lige nu som opvasker på restauranten Gringas, hvilket jeg virkelig værdsætter, da jeg synes, at arbejdsmiljøet er fantastisk.</p>
-            <p className="mb-4">Tidligere har jeg arbejdet som ketcher-opstrenger hos Ketshop, hvor jeg også vi iblandt gode kollegaer.</p>
+            <p className="mb-4">Jeg har tidligere arbejdet som opstrenger hos Ketshop, hvor jeg også var iblandt nogle gode kollegaer.</p>
             <p className="mb-4">Jeg trives bedst, når jeg arbejder i et positivt arbejdsmiljø, og jeg er god til at overholde mine forpligtelser, især når det drejer sig om at møde til tiden.</p>
             <p className="mb-4">Min erfaring inden for programmering er begrænset, men jeg finder emnet meget interessant og er ivrig efter at lære mere.</p>
             <p className="mb-4">Min passion for IT opstod allerede, da jeg fik min første computer, og jeg har altid nydt at arbejde med teknologi.</p>
