b6d9337cbdf72894b01a75745364de70936a1865
half0150 <half0150@edu.sde.dk>
2024-04-02 14:23:43 +0200

    Updated page

--- src/app/page.js
+++ src/app/page.js
@@ -40,108 +40,126 @@ export default function Home() {
             />
           </div>
         </section>
-
         <section>
-          <div className="pb-8 text-center">
-            <h3 className="text-3xl py-1 pb-6">Erhvervserfaringer</h3>
-            <p>2022 – Nu Restaurant Gringas (Opvasker)</p>
-            <p>2020/10 – 2022/10 Sportsforretning i Odense (Opstrenger)</p>
-            <div className="lg:flex gap-10">
-              <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                <Image
-                  src={pin}
-                  alt="pin"
-                  width={100}
-                  height={100}
-                  className="inline"
-                />
-                <h3 className="text-lg font-medium pt-8 pb-2">
-                  IT-kompetencer
-                </h3>
-                <p className=" py-1">
-                  <p className="text-teal-600">C#:</p> Har arbejdet med console app og Wpf.
-                </p>
-                <p className=" py-1">
-                  <p className="text-teal-600">VmWare:</p> Har brugt VmWare på
-                  Grundforløbet.
-                </p>
-                <p className=" py-1">
-                  <p className="text-teal-600">Visual Code:</p> Har brugt det i
-                  undervisningen og bruger det i mange af mine projekter.
-                </p>
-                <p className=" py-1">
-                  <p className="text-teal-600">NetBeans:</p> Har brugt det til
-                  mine PHP-projekter.
-                </p>
-                <p className=" py-1">
-                  <p className="text-teal-600">Visual Studio:</p> Har brugt det
-                  i undervisningen og bruger det i mange af mine projekter.
-                </p>
-              </div>
-              <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                <Image
-                  src={phone}
-                  alt="phone"
-                  width={100}
-                  height={100}
-                  className="inline"
-                />
-                <h3 className="text-lg font-medium pt-8 pb-2">
-                  Kontakt
-                </h3>
-                <p className=" py-1">
-                  <ContactInfo />
-                </p>
-
-              </div>
-              <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                <Image
-                  src={chat}
-                  alt="chat"
-                  width={100}
-                  height={100}
-                  className="inline"
-                />
-                <h3 className="text-lg font-medium pt-8 pb-2">Sprog</h3>
-                <p className=" py-1">
-                  <p className="text-teal-600">Engelsk:</p> Kan skrive og
-                  kommunikere med andre.
-                </p>
-                <p className=" py-1">
-                  <p className="text-teal-600">Dansk:</p> Snakker flydende
-                  Dansk. kan skrive og kommunikere med andre.
-                </p>
-                <p className=" py-1">
-                  <p className="text-teal-600">Skandinavisk:</p> Kan forstå
-                  noget svensk
-                </p>
-              </div>
-              <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                <Image
-                  src={sun}
-                  alt="sun"
-                  width={100}
-                  height={100}
-                  className="inline"
-                />
-                <h3 className="text-lg font-medium pt-8 pb-2">
-                  Fritidsinteresser
-                </h3>
-                <p className=" py-1">
-                  <p className="text-teal-600">Badminton:</p>Har spillet badminton i ca. 10 år
-                </p>
-                <p className=" py-1">
-                  <p className="text-teal-600">Film & Serier</p>Kan godt lide at
-                  se krimi, gyser, komedie film & serier
-                </p>
-                <p className=" py-1">
-                  <p className="text-teal-600">Musik</p>Elsker at lytte til
-                  musik som fx The Beatles.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
+  <div className="pb-8 text-center">
+    <h3 className="text-3xl py-1 pb-6">Erhvervserfaringer</h3>
+    <p>2022 – Nu Restaurant Gringas (Opvasker)</p>
+    <p>2020/10 – 2022/10 Sportsforretning i Odense (Opstrenger)</p>
+    <div className="lg:flex gap-10">
+      <div className="text-center shadow-lg p-10 rounded-xl my-10">
+        <Image
+          src={pin}
+          alt="pin"
+          width={100}
+          height={100}
+          className="inline"
+        />
+        <h3 className="text-lg font-medium pt-8 pb-2">
+          IT-kompetencer
+        </h3>
+        <div className=" py-1">
+          <p className="text-teal-600">C#:</p> Har arbejdet med console app og Wpf.
+        </div>
+        <div className=" py-1">
+          <p className="text-teal-600">VmWare:</p> Har brugt VmWare på Grundforløbet.
+        </div>
+        <div className=" py-1">
+          <p className="text-teal-600">Visual Code:</p> Har brugt det i undervisningen og bruger det i mange af mine projekter.
+        </div>
+        <div className=" py-1">
+          <p className="text-teal-600">NetBeans:</p> Har brugt det til mine PHP-projekter.
+        </div>
+        <div className=" py-1">
+          <p className="text-teal-600">Visual Studio:</p> Har brugt det i undervisningen og bruger det i mange af mine projekter.
+        </div>
+      </div>
+      <div className="text-center shadow-lg p-10 rounded-xl my-10">
+        <Image
+          src={phone}
+          alt="phone"
+          width={100}
+          height={100}
+          className="inline"
+        />
+        <h3 className="text-lg font-medium pt-8 pb-2">
+          Kontakt
+        </h3>
+        <p className=" py-1">
+          <ContactInfo />
+        </p>
+      </div>
+      <div className="text-center shadow-lg p-10 rounded-xl my-10">
+        <Image
+          src={chat}
+          alt="chat"
+          width={100}
+          height={100}
+          className="inline"
+        />
+        <h3 className="text-lg font-medium pt-8 pb-2">Sprog</h3>
+        <p className=" py-1">
+          <p className="text-teal-600">Engelsk:</p> Kan skrive og kommunikere med andre.
+        </p>
+        <p className=" py-1">
+          <p className="text-teal-600">Dansk:</p> Snakker flydende Dansk. kan skrive og kommunikere med andre.
+        </p>
+        <p className=" py-1">
+          <p className="text-teal-600">Skandinavisk:</p> Kan forstå noget svensk
+        </p>
+      </div>
+      <div className="text-center shadow-lg p-10 rounded-xl my-10">
+        <Image
+          src={sun}
+          alt="sun"
+          width={100}
+          height={100}
+          className="inline"
+        />
+        <h3 className="text-lg font-medium pt-8 pb-2">
+          Fritidsinteresser
+        </h3>
+        <p className=" py-1">
+          <p className="text-teal-600">Badminton:</p>Har spillet badminton i ca. 10 år
+        </p>
+        <p className=" py-1">
+          <p className="text-teal-600">Film & Serier</p>Kan godt lide at se krimi, gyser, komedie film & serier
+        </p>
+        <p className=" py-1">
+          <p className="text-teal-600">Musik</p>Elsker at lytte til musik som fx The Beatles.
+        </p>
+      </div>
+    </div>
+    <h3 className="text-3xl py-1 pb-6">Projekter</h3>
+    <div className="lg:flex justify-center gap-10"> {/* Modified */}
+      <div className="text-center shadow-lg p-10 rounded-xl my-10">
+        <Image
+          src={chat}
+          alt="chat"
+          width={100}
+          height={100}
+          className="inline"
+        />
+        <h3 className="text-lg font-medium pt-8 pb-2">Kundedatabase</h3>
+        <p className="py-1">
+          Udviklet en ny databasestruktur for KaosTek, herunder design, oprettelse og SQL-query til rapportgenerering af kundeordrer.
+        </p>
+      </div>
+      <div className="text-center shadow-lg p-10 rounded-xl my-10">
+        <Image
+          src={chat}
+          alt="chat"
+          width={100}
+          height={100}
+          className="inline"
+        />
+        <h3 className="text-lg font-medium pt-8 pb-2">Digitalisering af kajakklubbens opslagstavle</h3>
+        <p className="py-1">
+          Oprettet relationel database for Kajakklubben Pagaj med ER-diagram, implementering af tabeller og testdata til SQL-forespørgsler.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
       </main>
     </div>
   );
