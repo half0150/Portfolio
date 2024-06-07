368a7092e9e30c98a71700717d7a860ccabedb20
half0150 <half0150@edu.sde.dk>
2024-06-07 13:34:15 +0200

    Added Speed Insights (Vercel)

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "../app/Navbar";
 import Footer from "../app/footer";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,8 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>
-      <GoogleAnalytics gaId="G-SFNND2N4NF" />
+        <SpeedInsights />
+        <GoogleAnalytics gaId="G-SFNND2N4NF" />
         <Navbar />
         {children}
         <Footer />
